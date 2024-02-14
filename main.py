deee0e11ff3f792ead32221eaafad21d472053fd
mickdiggin <m_degan@mail.fhsu.edu>
2024-02-14 15:48:50 -0600

    Refactored code to more closely align with new part 2 video.

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-# INF601 - Advanced Programming in Python
-# Michael DeGan
-# Mini Project 1
-
-# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
-# (5/5 points) Proper import of packages used.
-# (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
-# (10/10 points) Store this information in a list that you will convert to a array in NumPy.
-# (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
-# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
-# (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
-# (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
-
 # Suppressing all FutureWarnings because they are cluttering the console.
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -19,6 +5,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 from pathlib import Path
+# INF601 - Advanced Programming in Python
+# Michael DeGan
+# Mini Project 1
+
+# (5/5 points) Initial comments with your name, class and project at the top of your .py file.
+# (5/5 points) Proper import of packages used.
+# (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock
+# tickers for the last 10 trading days.
+# (10/10 points) Store this information in a list that you will convert to an array in NumPy.
+# (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the
+# documentation for matplotlib. At minimum it just needs to show 10 data points.
+# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder,
+# the project should save these when it executes. You may want to add this folder to your .gitignore file.
+# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+# (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt
+# file which contains all the packages that need installed. You can create this fille with the output of pip freeze at
+# the terminal prompt.
+# (20/20 points) There should be a README.md file in your project that explains what your project is, how to install
+# the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
+
+
 def getClosing(ticker):
     # Get the closing price for the last 10 trading days
     stock = yf.Ticker(ticker)
@@ -57,7 +64,7 @@ def printGraph(stock):
     # Set our labels for the graph
     plt.xlabel("Days")
     plt.ylabel("Closing Price")
-    plt.title("Closing Price for " + stock)
+    plt.title(f"{stock} Closing Price")
 
     # Saves plot
     saveFile = "charts/" + stock + ".png"
@@ -66,27 +73,28 @@ def printGraph(stock):
     # Show the graph
     plt.show()
 
-def getStocks():
-    stocks = []
-
-    print("Please enter 5 stocks to graph:")
-    for i in range(1, 6):
-        while True:
-            print("Enter stock ticker " + str(i))
-            userValue = input("> ")
-            stock = yf.Ticker(userValue)
-            print("Checking ticker...")
-
-            # This section had to be refactored because try-except statements cannot handle the various types of errors
-            # that Ticker.info throws. Note: stock.info will always have 1 entry, even for invalid ticker symbols.
-            if len(stock.info) > 1:
-                stocks.append(userValue)
-                print("Valid input.")
-                break
-            else:
-                print("Invalid ticker. Please try again.")
-
-    return stocks
+# Old getStocks() function to collect user input.
+# def getStocks():
+#     stocks = []
+#
+#     print("Please enter 5 stocks to graph:")
+#     for i in range(1, 6):
+#         while True:
+#             print("Enter stock ticker " + str(i))
+#             userValue = input("> ")
+#             stock = yf.Ticker(userValue)
+#             print("Checking ticker...")
+#
+#             # This section had to be refactored because try-except statements cannot handle the various types of errors
+#             # that Ticker.info throws. Note: stock.info will always have 1 entry, even for invalid ticker symbols.
+#             if len(stock.info) > 1:
+#                 stocks.append(userValue)
+#                 print("Valid input.")
+#                 break
+#             else:
+#                 print("Invalid ticker. Please try again.")
+#
+#     return stocks
 
 # Start of program
 try:
@@ -96,6 +104,9 @@ except FileExistsError:
     pass
 
 #Collect and validate symbols from end-user, output as graphs, store in directory "charts."
-for stock in getStocks():
+
+theStocks = ["GOOG", "COST", "WMT", "SMSN.L", "LYFT"]
+
+for stock in theStocks:
     getClosing(stock)
     printGraph(stock)
